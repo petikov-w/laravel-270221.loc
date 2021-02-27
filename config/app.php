e591a675991cf4e77dd1ccee92f531aab521482f
Petikov Vladimir <vx7xv@inbox.ru>
2021-02-27 15:03:21 +0300

    Коммит 2 - 270221-1502

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
+
         /*
          * Package Service Providers...
          */
@@ -174,6 +176,13 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         *  Мои Service Providers для этого роекта
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
+
+        //=============================================
 
     ],
 
