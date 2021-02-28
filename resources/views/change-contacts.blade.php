de92dc63f03b88d07dd9dcf03abe23c059d87ebd
Petikov Vladimir <vx7xv@inbox.ru>
2021-02-28 12:15:31 +0300

    Коммит 4 - 280221-1215

--- resources/views/change-contacts.blade.php
+++ resources/views/change-contacts.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 
-    <div class="s-header">{{$message}}</div>
-
+<div class="s-header">{{$message}}</div>
+<div class="errors-box">
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul class="alert">
@@ -15,19 +15,19 @@
             </ul>
         </div>
     @endif
-
+</div>
     <!--========================== ФОРМА ИЗМЕНЕНИЯ КОНТАКТНЫХ ДАННЫХ ===========================-->
-
-           <form method="post" action="{{ route('admin.store') }}" class="form-dsg">
-               @csrf
-               <label for="telefon" class="form-label">Телефон</label>
-{{--               <input name="telefon" id="telefon" type="text" class="form-input" value="" placeholder="Введите телефон">--}}
-               <input name="telefon" id="telefon" type="text" class="form-input" value="{{$telefon}}" placeholder="Введите телефон">
-               <label for="email" class="form-label">Адрес электронной почты</label>
-{{--               <input name="email" id="email" type="text" class="form-input" value="" placeholder="Введите email">--}}
-               <input name="email" id="email" type="text" class="form-input" value="{{$email}}" placeholder="Введите email">
-               <button type="submit" class="form-submit" >Изменить</button>
-            </form>
-
+<div class="form-contact">
+    <form method="post" action="{{ route('admin.store') }}" class="form-dsg">
+        @csrf
+        <label for="telefon" class="form-label">Телефон</label>
+        <input name="telefon" id="telefon" type="text" class="form-input"
+               value="{{$telefon}}" placeholder="Введите телефон">
+        <label for="email" class="form-label">Адрес электронной почты</label>
+        <input name="email" id="email" type="text" class="form-input"
+               value="{{$email}}" placeholder="Введите email">
+        <button type="submit" class="form-submit" >Изменить</button>
+    </form>
+</div>
     <!--=======================================================================================-->
 @endsection
